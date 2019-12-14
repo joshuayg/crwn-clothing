64f5216e1f9ae9b0b408acb34ef6bbf1c362bc53
Joshua Yang <joshua.yang@gettyimages.com>
2019-12-13 16:05:16 -0800

    firebase authenticate with email

--- src/App.js
+++ src/App.js
@@ -21,10 +21,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-      // this.setState({ currentUser: user });
-      // console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+        (await userRef).onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => { console.log(this.state) })
+        })
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
