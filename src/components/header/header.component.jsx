2217584ffa86921b5be44b739e3dd31e5ce25d73
Joshua Yang <joshua.yang@gettyimages.com>
2019-12-20 14:10:59 -0800

    finish wiring up redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.util';
@@ -26,4 +27,10 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    // from reducer
+    currentUser: state.user.currentUser
+})
+
+// getting the currentUser from reducer and can remove injection from App.js
+export default connect(mapStateToProps)(Header);
